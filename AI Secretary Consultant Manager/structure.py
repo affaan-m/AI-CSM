9732d393d27a4dcb8271cb0e258d3be543f4a7f2
Affaan Mustafa <124439313+affaan-m@users.noreply.github.com>
2023-09-18 16:30:12 -0700

    Updates to secretary
    Extended section three nearing completion.

--- AI Secretary Consultant Manager/structure.py
+++ AI Secretary Consultant Manager/structure.py
@@ -1,6 +1,7 @@
 import os
 import json
 import logging
+from pydoc import pager
 import openai
 import redis
 from googleapiclient.discovery import build
@@ -289,33 +290,198 @@ import json
 import re
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.metrics.pairwise import cosine_similarity
+import requests
+from bs4 import BeautifulSoup
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+import numpy as np
+from numpy.linalg import norm
+import pandas as pd
+import openai
 
+def load_text(source_type, source):
+    """
+    Load text data from various sources: URL, direct upload, or Google Drive.
+
+    Args:
+    source_type (str): The type of the source ('url', 'file', 'gdrive', 'direct_text')
+    source (str): The source input. Depending on the source_type, it could be a URL, file path, Google Drive ID, or direct text.
+
+    Returns:
+    str: The loaded text data.
+    """
+    text = ""
+
+    if source_type == 'url':
+        # Load text from URL
+        response = requests.get(source)
+        soup = BeautifulSoup(response.content, "html.parser")
+        text = soup.get_text()
+        text = text.replace("\n", " ")
+
+    elif source_type == 'file':
+        # Load text from a local file
+        with open(source, 'r', encoding='utf-8') as file:
+            text = file.read()
+
+    elif source_type == 'gdrive':
+        # Load text from Google Drive (assuming the file is publicly accessible)
+        # The source should be the file ID
+        gdrive_url = f"https://drive.google.com/uc?export=download&id={source}"
+        response = requests.get(gdrive_url)
+        text = response.text
+
+    elif source_type == 'direct_text':
+        # Direct text input
+        text = source
+
+    return text
+
+
+def split_text(text):
+    """
+    Split the loaded text into smaller chunks using RecursiveCharacterTextSplitter.
+
+    Args:
+    text (str): The text to be split.
+
+    Returns:
+    list: A list of split texts.
+    """
+    text_splitter = RecursiveCharacterTextSplitter(    
+        chunk_size = 500,
+        chunk_overlap = 100,
+        length_function = len,
+    )
+
+    texts = text_splitter.create_documents([text])
+    return texts
+
+
+# Usage example
+source_type = 'url'  # Change this to 'file', 'gdrive', or 'direct_text' based on your source
+source = 'https://en.wikipedia.org/wiki/OpenAI'  # Change this to your source (URL, file path, Google Drive ID, or direct text)
+
+text = load_text(source_type, source)
+texts = split_text(text)
+
+# Now texts contain the split texts ready to be processed further
+from langchain.embeddings.openai import OpenAIEmbeddings
+import redis
+import json
+
+# Azure Redis Cache configuration (replace with your actual settings)
+REDIS_HOST = 'your-azure-redis-url'
+REDIS_PORT = 'your-azure-redis-port'
+REDIS_DB = 0
+REDIS_PASSWORD = 'your-azure-redis-password'
+
+# Initialize Redis client
+redis_client = redis.Redis(host=REDIS_HOST, port=REDIS_PORT, db=REDIS_DB, password=REDIS_PASSWORD)
+
+# Define the embeddings model using Langchain's OpenAIEmbeddings
+embeddings_model = OpenAIEmbeddings()
+
+def store_embeddings_in_redis(texts, embeddings_model, redis_client):
+    """
+    Compute embeddings for a list of texts and store them in Redis.
+
+    Args:
+    texts (list of str): The texts for which to compute the embeddings.
+    embeddings_model (OpenAIEmbeddings): The embeddings model.
+    redis_client (redis.Redis): The Redis client.
+
+    Returns:
+    None
+    """
+    for idx, text in enumerate(texts):
+        # Compute the embedding for the current text
+        embedding = embeddings_model.compute_embedding(text)
+        
+        # Convert the embedding array to a JSON string
+        embedding_json = json.dumps(embedding.tolist())
+        
+        # Store the embedding JSON string in Redis with a unique key
+        redis_client.set(f"text_embedding:{idx}", embedding_json)
+
+# Usage:
+# Assuming `texts` is a list of texts obtained from the previous step (split_text function)
+store_embeddings_in_redis(texts, embeddings_model, redis_client)
+
+
+# Initializing RedisResultsStorage with Azure Redis Cache settings
+# You would replace 'your-azure-redis-url' with your actual Azure Redis URL and 'your-azure-redis-port' with your actual port.
 class RedisResultsStorage:
-    def __init__(self, host='localhost', port=6379, db=0):
-        self.client = redis.Redis(host=host, port=port, db=db)
+    def __init__(self, host='your-azure-redis-url', port='your-azure-redis-port', db=0, password='your-azure-redis-password'):
+        self.client = redis.Redis(host=host, port=port, db=db, password=password)
 
     def store_result(self, key, value):
-        self.client.set(key, value)
+        self.client.set(key, json.dumps(value))
 
     def get_result(self, key):
-        return self.client.get(key)
+        result = self.client.get(key)
+        return json.loads(result) if result else None
+
 
 # Custom Prompt Templates
+class FunctionExplainerPromptTemplate:
+    def __init__(self, input_variables):
+        self.input_variables = input_variables
+
+    def generate_prompt(self, **kwargs):
+        prompt = "Explain the function: "
+        for var in self.input_variables:
+            prompt += f"\n{var}: {kwargs.get(var, '')}"
+        return prompt
+
+class ChatPromptTemplate:
+    @classmethod
+    def from_messages(cls, messages):
+        instance = cls()
+        instance.messages = messages
+        return instance
+
+    def generate_prompt(self, user_input):
+        conversation = ""
+        for msg in self.messages[:-1]:
+            conversation += f"{msg[0]}: {msg[1]}\n"
+        conversation += f'human: {user_input}'
+        return conversation
+
+# Initializing instances of the prompt templates with predefined parameters
 function_explainer_template = FunctionExplainerPromptTemplate(input_variables=["function_name"])
 chat_template = ChatPromptTemplate.from_messages([
-    ("system", "You are a helpful AI bot named Bob."),
-    ("human", "Hello, how are you doing?"),
-    ("ai", "I'm doing well, thanks!"),
+    ("system", "You are a multifunctional AI, equipped to assist with a range of tasks."),
+    ("human", "Hello, how can you assist me today?"),
+    ("ai", "Hello! I can help you with scheduling, sending emails, managing Trello tasks, and much more. Please provide the details of the task you'd like assistance with."),
     ("human", "{user_input}"),
 ])
 
 # Examples for dynamic few-shot prompting
 examples = [
+    # Calendar Scheduling
     "Schedule a meeting with {participants} on {date} at {time} in {location}.",
-    "Create a Trello task at {board} with description {description} with title {title} with tags {tags}.",
-    "Add meeting to calendar at {date} and {time}.",
-    "Email participant with subject {subject} message {message}.",
-    # Add more examples as needed
+    "Reschedule the meeting on {date} to {new_date} at {new_time}.",
+    "Cancel the meeting scheduled on {date}.",
+    
+    # Email Sending
+    "Draft an email to {recipient} with subject {subject} and body {body}.",
+    "Send an email to {recipient} with subject {subject} and attachment {attachment_path}.",
+    "Set up an auto-reply message with the text {message}.",
+    
+    # Slack Message Sending
+    "Send a Slack message to {channel_name} with the message {message_content}.",
+    "Create a Slack poll in {channel_name} with the question {question} and options {options}.",
+    "Schedule a Slack message to {recipient} on {date} at {time} with the message {message_content}.",
+    
+    # Trello Task Creating
+    "Create a Trello task at {board} with description {description}, title {title}, and tags {tags}.",
+    "Update Trello task {task_id} with new description {description}.",
+    "Add a comment to Trello task {task_id} with the message {message}.",
+    
+    # Document Generating/Saving
+    "Create a {document_type} document titled {title} with the following content {content}.",
+    "Save the document titled {title} to {storage_service}.",
+    "Share the document titled {title} with {recipient_email}.",
 ]
 
 # Example Selector Class
@@ -331,16 +497,46 @@ class ExampleSelector:
         most_similar_index = similarities.argmax()
         return self.examples[most_similar_index]
 
+# Function to create a prompt using the Example Selector
+def create_prompt(user_input):
+    # Initialize the Example Selector with predefined examples
+    example_selector = ExampleSelector(examples)
+    
+    # Select the most appropriate example based on the user input
+    selected_example = example_selector.select_example(user_input)
+    
+    # Here, you would replace the placeholders in the selected example with actual values from the user input
+    # This part of the code would depend on how exactly the user input is structured and how you can extract the necessary information from it
+    
+    # Returning the selected example for now
+    return selected_example
+
 # Placeholder function to simulate LLM invocation
 def invoke_llm(prompt):
+    # Eventually, this function will call your LLM to generate a response based on the prompt
+    # You might replace this with a call to an API endpoint or a library function
     return f"Generated response for: {prompt}"
 
 # Function to generate a response using LLM
 def generate_response(query, examples):
+    # Initialize the Example Selector with predefined examples
     example_selector = ExampleSelector(examples)
+    
+    # Select the most appropriate example based on the query
     selected_example = example_selector.select_example(query)
+    
+    # Replace placeholders in the selected example with actual values from the query
+    # NOTE: The placeholder replacement logic will depend on how you can extract the necessary data from the query
+    # Here we are assuming that `query` is a dictionary with keys matching the placeholder names
+    for placeholder, value in query.items():
+        selected_example = selected_example.replace(f"{{{placeholder}}}", value)
+    
+    # Compose the final prompt to be sent to the LLM
     composed_prompt = selected_example
+    
+    # Invoke the LLM with the composed prompt
     response = invoke_llm(composed_prompt)
+    
     return response
 
 # Input Parser: Meeting Request
@@ -380,29 +576,44 @@ def serialize_prompt(prompt, metadata=None):
 # Function for Prompt Pipelining
 def process_prompt_pipeline(prompts, redis_storage):
     results = []
+    # The Example Selector instance should be initialized with a predefined set of examples 
+    example_selector = ExampleSelector(examples)
+    
     for prompt_data in prompts:
         prompt_json = json.loads(prompt_data)
         prompt = prompt_json['prompt']
         metadata = prompt_json['metadata']
         result = ""
 
+        # Step 2: Depending on the type of prompt, parse it to extract necessary details
         if metadata.get('type') == 'meeting_request':
             meeting_data = parse_meeting_request(prompt)
-            query = compose_meeting_prompt(**meeting_data)
-            result = generate_response(query, examples)
-            result = format_meeting_confirmation(result)
-        # Add other conditions for handling different types of prompts
+            composed_prompt = compose_meeting_prompt(**meeting_data)
         else:
-            result = "Processed for other types."
+            composed_prompt = prompt  # For now, using the prompt directly
+            
+        # Step 3: Select a suitable example based on the composed prompt
+        selected_example = example_selector.select_example(composed_prompt)
+
+        # Step 4: Generate a response using LLM
+        response = invoke_llm(selected_example)
+        
+               # Step 5: Format the raw response received from the LLM
+        if metadata.get('type') == 'meeting_request':
+            response = format_meeting_confirmation(response)
+        else:
+            # Additional formatting for other types of responses
+            pass
 
         # Store the result in Redis
         key = metadata.get('key')
-        redis_storage.store_result(key, result)
+        redis_storage.store_result(key, response)  # Updated this line to store the response instead of result
 
         results.append({
-            'result': result,
+            'result': response,  # Updated this line to append the response instead of result
             'metadata': metadata
         })
+
     return results
 
 # Usage
