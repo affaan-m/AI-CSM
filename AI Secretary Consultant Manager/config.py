742b29e7b194c0e1b765d86455656499e4890e3a
Affaan Mustafa <124439313+affaan-m@users.noreply.github.com>
2023-09-21 16:07:32 -0700

    Update config.py

--- AI Secretary Consultant Manager/config.py
+++ AI Secretary Consultant Manager/config.py
@@ -19,13 +19,12 @@ CONFIG = {
     },
     "google_api": {
         "client_id": "35133980476-l14hfrntg9f96n2omvme09f58r98b0tn.apps.googleusercontent.com",
-        "client_secret": "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"
+        "client_secret": "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",
     },
     "microsoft_api": {
             "client_id": "4ld8Q~KT5XMRFhzV2N~2WEAVNDF1~MZbCBdcHbNL",
             "client_secret": "7018f836-4f2d-41e1-9a26-19f381ee9521",
             "redirect_uri": "https://dcube.ai",
-            "refresh_token": "your-refresh-token",
         }
     },
 }
