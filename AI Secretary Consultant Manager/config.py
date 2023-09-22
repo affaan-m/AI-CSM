b78944b2edd5f7eb494864cb26c0de96a2216042
Affaan Mustafa <124439313+affaan-m@users.noreply.github.com>
2023-09-21 17:13:34 -0700

    Update config.py

--- AI Secretary Consultant Manager/config.py
+++ AI Secretary Consultant Manager/config.py
@@ -20,11 +20,28 @@ CONFIG = {
     "google_api": {
         "client_id": "35133980476-l14hfrntg9f96n2omvme09f58r98b0tn.apps.googleusercontent.com",
         "client_secret": "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",
+        "scopes": [
+            "https://www.googleapis.com/auth/gmail.modify",
+            "https://www.googleapis.com/auth/drive",
+            "https://www.googleapis.com/auth/calendar",
+            "https://www.googleapis.com/auth/contacts.readonly"
+        ],
+        "token_file": "token.json"
     },
     "microsoft_api": {
-            "client_id": "4ld8Q~KT5XMRFhzV2N~2WEAVNDF1~MZbCBdcHbNL",
-            "client_secret": "7018f836-4f2d-41e1-9a26-19f381ee9521",
-            "redirect_uri": "https://dcube.ai",
-        }
-    },
+        "client_id": "4ld8Q~KT5XMRFhzV2N~2WEAVNDF1~MZbCBdcHbNL",
+        "client_secret": "7018f836-4f2d-41e1-9a26-19f381ee9521",
+        "redirect_uri": "https://dcube.ai",
+        "tenant": "common",
+        "scopes": [
+            "https://graph.microsoft.com/Mail.ReadWrite",
+            "https://graph.microsoft.com/Mail.Send",
+            "https://graph.microsoft.com/Calendars.ReadWrite",
+            "https://graph.microsoft.com/Files.ReadWrite",
+            "https://graph.microsoft.com/Contacts.ReadWrite",
+            "https://graph.microsoft.com/User.Read",
+            "https://graph.microsoft.com/offline_access",
+            "https://graph.microsoft.com/openid"
+        ]
+    }
 }
